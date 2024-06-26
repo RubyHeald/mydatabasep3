015e8699ec826d6c278e720b596284207377be56
rubyheald <ruby.heald@barclays.com>
2024-06-26 13:49:54 +0100

    H2 Database linked with Employee DB

--- src/main/java/com/newdatabase/mydatabase/controller/EmployeeController.java
+++ src/main/java/com/newdatabase/mydatabase/controller/EmployeeController.java
@@ -6,18 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
+@RequestMapping("/employees")
 @Controller
 public class EmployeeController {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository repository;
 
-    @GetMapping("/employees")
-    public String listEmployees(Model model) {
-        List<Employee> employees = employeeRepository.findAll();
-        model.addAttribute("employees", employees);
-        return "employees";
+    //constructor parameter
+
+    public EmployeeController (EmployeeRepository repository){
+        this.repository = repository;
     }
+//    @GetMapping ()
+//    public List<Employee> findAll(){
+//        return repository.findAll();
+//    }
 }
--- src/main/java/com/newdatabase/mydatabase/controller/HomeController.java
+++ src/main/java/com/newdatabase/mydatabase/controller/HomeController.java
@@ -1,13 +1,31 @@
 package com.newdatabase.mydatabase.controller;
 
+import com.newdatabase.mydatabase.model.Employee;
+import com.newdatabase.mydatabase.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/Employees")
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public String getEmployeeList(Model model) {
+        List<Employee> employees = employeeRepository.findAll();
+        model.addAttribute("employees", employees);
+        System.out.println("Hello");
+        return "employees"; // This returns the employee.html template
+    }
+
+    @GetMapping("/")
     public String home() {
-        return "index"; // This returns the index.html template
+        return "index";
     }
 }
+
