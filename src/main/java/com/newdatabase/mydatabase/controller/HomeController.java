ba6ade181dbd168446d97229d88a3d8348e6df44
rubyheald <ruby.heald@barclays.com>
2024-06-26 14:28:03 +0100

    H2 Database linked with Employee DB add & delete form

--- src/main/java/com/newdatabase/mydatabase/controller/HomeController.java
+++ src/main/java/com/newdatabase/mydatabase/controller/HomeController.java
@@ -5,7 +5,7 @@ import com.newdatabase.mydatabase.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,13 +19,30 @@ public class HomeController {
     public String getEmployeeList(Model model) {
         List<Employee> employees = employeeRepository.findAll();
         model.addAttribute("employees", employees);
-        System.out.println("Hello");
-        return "employees"; // This returns the employee.html template
+        return "employees"; // Returns employees.html
     }
 
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "index"; // returns index.html
+    }
+
+    @GetMapping("/employees/add")
+    public String showAddEmployeeForm(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "addEmployee"; // Returns addEmployee.html
+    }
+
+    @PostMapping("/employees/add")
+    public String addEmployee(@ModelAttribute Employee employee) {
+        employeeRepository.save(employee);
+        return "redirect:/employees"; // Back to the employee list view
+    }
+    @GetMapping("/employees/delete/{id}")
+    public String deleteEmployee(@PathVariable("id") Long id) {
+        employeeRepository.deleteById(id);
+        return "redirect:/employees"; // Back to the employee list view
     }
 }
+
 
